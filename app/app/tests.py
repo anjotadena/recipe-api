105a33ed971e2bd29860a314bf80c1dc6b5d4a3a
Anjo Tadena <tadena.anjo@gmail.com>
2021-09-08 20:41:00 -0600

    Create basic unit test

---
+++ app/app/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from app.calc import add
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """Test that two number are added together"""
+        self.assertEqual(add(5, 5), 10)
