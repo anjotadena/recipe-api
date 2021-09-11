886e14fb75016b29109d864bd3728492e9e57e11
Anjo Tadena <tadena.anjo@gmail.com>
2021-09-10 18:53:16 -0600

    Fix core migrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-# Create your models here.
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
